0029d2d953394d936ef29376dac930e33f07a606
Michaela <Michaela-13>
2022-06-17 15:39:20 +0200

    first

---
+++ code.js/code.js
@@ -0,0 +1,13 @@
+// const numbers=[1,2,3,];
+
+// const iterator=Array.values();
+// let one=iterator.next().value;
+// let two=iterator.next().value;
+
+// console.log(one);
+// console.log(two);
+
+
+let number=[2,3,4,5,6];
+let iterNum=number.values();
+console.log(iterNum.next().value);
